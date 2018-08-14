fd0540c839e254034a480ce9c3b11b391577d7ee
Øystein Heggen <oystein.heggen@gmail.com>
2018-08-14 21:20:12 +0200

    log

--- pysio/main.py
+++ pysio/main.py
@@ -8,10 +8,14 @@ if __name__ == '__main__':
     t = temperature_humidity.th02()
     h = barometer.hp206c()
 
-    print("Temp: %.2fC\tHumidity:%.2f" %
+    with open('/home/pi/log.csv','a') as logfile:
+         logfile.write(t.getTemperature() + "," + t.getHumidity())
+    
+    
+  """   print("Temp: %.2fC\tHumidity:%.2f" %
         (t.getTemperature(), t.getHumidity()), "%")
     temp = h.ReadTemperature()
     pressure = h.ReadPressure()
     altitude = h.ReadAltitude()
     print("Temperature\t: %.2f C\nPressure\t: %.2f hPa\nAltitude\t: %.2f m" %
-        (temp, pressure, altitude))
+        (temp, pressure, altitude)) """
