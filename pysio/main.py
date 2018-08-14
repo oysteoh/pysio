89a6db3270893d82f00f0dfcd2f090a4759dbf7b
Øystein Heggen <oystein.heggen@gmail.com>
2018-08-14 21:02:25 +0200

    structure

---
+++ pysio/main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import sys
+from lib import barometer
+from lib import temperature_humidity
+
+if __name__ == '__main__':
+    t = temperature_humidity.th02()
+    h = barometer.hp206c()
+
+    print("Temp: %.2fC\tHumidity:%.2f" %
+        (t.getTemperature(), t.getHumidity()), "%")
+    temp = h.ReadTemperature()
+    pressure = h.ReadPressure()
+    altitude = h.ReadAltitude()
+    print("Temperature\t: %.2f C\nPressure\t: %.2f hPa\nAltitude\t: %.2f m" %
+        (temp, pressure, altitude))
