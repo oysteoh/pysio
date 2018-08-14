3d419dec876cc2ea8d63a5ade75f6e2acb29608f
Øystein Heggen <oystein.heggen@gmail.com>
2018-08-14 21:32:02 +0200

    added other sensor

--- pysio/main.py
+++ pysio/main.py
@@ -9,16 +9,8 @@ if __name__ == '__main__':
     t = temperature_humidity.th02()
     h = barometer.hp206c()
 
-    row = [t.getTemperature(), t.getHumidity()]
+    row = [t.getTemperature(), h.ReadTemperature(), t.getHumidity(), h.ReadPressure()]
     with open('/home/pi/log.csv','a') as logfile:
         writer = csv.writer(logfile)
         writer.writerow(row)
 
-    
-    #print("Temp: %.2fC\tHumidity:%.2f" %
-    #    (t.getTemperature(), t.getHumidity()), "%")
-    #temp = h.ReadTemperature()
-    #pressure = h.ReadPressure()
-    #altitude = h.ReadAltitude()
-    #print("Temperature\t: %.2f C\nPressure\t: %.2f hPa\nAltitude\t: %.2f m" %
-    #    (temp, pressure, altitude))
