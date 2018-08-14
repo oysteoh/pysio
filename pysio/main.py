7b26c481267c1a201104209083d0e862a9f1d7f9
Øystein Heggen <oystein.heggen@gmail.com>
2018-08-14 22:19:00 +0200

    added date

--- pysio/main.py
+++ pysio/main.py
@@ -2,6 +2,7 @@
 
 import sys
 import csv
+import datetime
 from lib import barometer
 from lib import temperature_humidity
 
@@ -9,7 +10,7 @@ if __name__ == '__main__':
     t = temperature_humidity.th02()
     h = barometer.hp206c()
 
-    row = [t.getTemperature(), h.ReadTemperature(), t.getHumidity(), h.ReadPressure()]
+    row = [datetime.datetime.utcnow(), t.getTemperature(), h.ReadTemperature(), t.getHumidity(), h.ReadPressure()]
     with open('/home/pi/log.csv','a') as logfile:
         writer = csv.writer(logfile)
         writer.writerow(row)
