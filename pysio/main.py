97f5b18ffcb303230f9950519b544fd455ead48e
Øystein Heggen <oystein.heggen@gmail.com>
2018-10-01 20:49:38 +0200

    missing comma

--- pysio/main.py
+++ pysio/main.py
@@ -29,7 +29,7 @@ if __name__ == '__main__':
             "room": "shed"
         },
         "fields": {
-            "value_1": t.getTemperature()
+            "value_1": t.getTemperature(),
             "value_2": h.ReadTemperature()
         }
     }, {
