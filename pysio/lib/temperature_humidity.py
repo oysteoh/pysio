a23353c5a503c83c373013d5e4dbf8ea6b60fab8
Øystein Heggen <oystein.heggen@gmail.com>
2018-10-09 20:52:50 +0200

    bug

--- pysio/lib/temperature_humidity.py
+++ pysio/lib/temperature_humidity.py
@@ -77,7 +77,7 @@ class th02:
 		t_raw=bus.read_i2c_block_data(self.ADDRESS, self.TH02_REG_DATA_H,3)
 		if debug:
 			print(t_raw)
-		temperature = (t_raw[1]<<8|t_raw[2])>>2
+		temperature = t_raw >> 2
 		return (temperature/32.0)-50.0
 		
 	def getHumidity(self):
