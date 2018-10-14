bcfc48c32f437987d084bac9f773de8517eee2cf
Øystein Heggen <oystein.heggen@gmail.com>
2018-10-14 13:21:28 +0200

    print

--- pysio/main.py
+++ pysio/main.py
@@ -10,40 +10,43 @@ from lib import temperature_humidity
 if __name__ == '__main__':
     t = temperature_humidity.th02()
     h = barometer.hp206c()
-    time = datetime.datetime.utcnow()
-    dbclient = InfluxDBClient('10.0.0.2', 8086, 'root', 'root', 'pysio')
 
-    json_body = [{
-        "measurement": "humidity",
-        "tags": {
-            "place": "home",
-            "room": "shed"
-        },
-        "fields": {
-            "value": t.getHumidity()
-        }
-    }, {
-        "measurement": "temperature",
-        "tags": {
-            "place": "home",
-            "room": "shed"
-        },
-        "fields": {
-            "value_1": t.getTemperature(),
-            "value_2": h.ReadTemperature()
-        }
-    }, {
-        "measurement": "pressure",
-        "tags": {
-            "place": "home",
-            "room": "shed"
-        },
-        "fields": {
-            "value": h.ReadPressure()
-        }
-    }]
+    print(t.getTemperature())
+    print(t.getHumidity())
+    # time = datetime.datetime.utcnow()
+    # dbclient = InfluxDBClient('10.0.0.2', 8086, 'root', 'root', 'pysio')
+# 
+    # json_body = [{
+    #     "measurement": "humidity",
+    #     "tags": {
+    #         "place": "home",
+    #         "room": "shed"
+    #     },
+    #     "fields": {
+    #         "value": t.getHumidity()
+    #     }
+    # }, {
+    #     "measurement": "temperature",
+    #     "tags": {
+    #         "place": "home",
+    #         "room": "shed"
+    #     },
+    #     "fields": {
+    #         "value_1": t.getTemperature(),
+    #         "value_2": h.ReadTemperature()
+    #     }
+    # }, {
+    #     "measurement": "pressure",
+    #     "tags": {
+    #         "place": "home",
+    #         "room": "shed"
+    #     },
+    #     "fields": {
+    #         "value": h.ReadPressure()
+    #     }
+    # }]
 
-    dbclient.write_points(json_body)
+    # dbclient.write_points(json_body)
 
     # row = [datetime.datetime.utcnow(), t.getTemperature(), h.ReadTemperature(), t.getHumidity(), h.ReadPressure()]
     # with open('/home/pi/log.csv','a') as logfile:
